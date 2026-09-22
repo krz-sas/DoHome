f22d559f8030a05769bfe382da7612c869e04c2b
Krzysztof Sasiak <142749040+krz-sas@users.noreply.github.com>
2026-09-22 21:33:26 +0200

    Adjust DoHome to latest HASS changes from 2026

--- DoHome_HassAssistant_Component/custom_components/dohome/light.py
+++ DoHome_HassAssistant_Component/custom_components/dohome/light.py
@@ -11,17 +11,44 @@ from homeassistant.helpers.event import track_time_interval
 import homeassistant.util.color as color_util
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
-    ATTR_COLOR_TEMP,
     ATTR_EFFECT,
     ATTR_RGBWW_COLOR,
-    PLATFORM_SCHEMA,
-    SUPPORT_BRIGHTNESS,
-    SUPPORT_COLOR,
     LightEntity,
+    LightEntityFeature,
     ColorMode
 )
 
 from . import (DOHOME_GATEWAY, DoHomeDevice)
+
+EFFECTS_DICT = {
+"Colorful gradient": 1,
+"red gradient": 2,
+"Green gradient": 3,
+"blue gradient": 4,
+"yellow gradient": 5,
+"Cyan gradient": 6,
+"Purple gradient": 7,
+"White gradient": 8,
+"Red strobe": 9,
+"Green strobe": 10,
+"Blue strobe": 11,
+"yellow strobe": 12,
+"Red-green gradient": 13,
+"red and blue gradient": 14,
+"Green-blue gradient": 15,
+"Red-green jump": 16,
+"Red and blue jump": 17,
+"Green-blue transition": 18,
+"Red and green strobe": 19,
+"red and blue strobe": 20,
+"Green and blue strobe": 21,
+"Colorful jump": 22,
+"Colorful strobe": 23,
+"White strobe": 24,
+"Three-color gradient": 25,
+"Three color jump": 26,
+"Three-color strobe": 27
+}
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -45,7 +72,7 @@ class DoHomeLight(DoHomeDevice, LightEntity):
         self._device = device
         self._state = False
         self._rgb = (0, 0, 0, 0, 0)
-        self._brightness = 100
+        self._brightness = 255
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         DoHomeDevice.__init__(self, device['name'], device)
@@ -62,24 +89,37 @@ class DoHomeLight(DoHomeDevice, LightEntity):
 
     @property
     def is_on(self):
+        return self._state
+
+    def update(self):
         """Return true if light is on."""
         data = { "cmd":25 }
         op = json.dumps(data)
 
         try:
             resp = self._send_cmd(self._device,'cmd=ctrl&devices={[' + self._device["sid"] + ']}&op=' + op + '}', 25)
-            self._rgb = (resp["r"], resp["g"], resp["b"], resp["w"], resp["m"])
+            self._rgb = (int(resp["r"]/self.brightness), 
+                    int(resp["g"]/self.brightness), 
+                    int(resp["b"]/self.brightness), 
+                    int(resp["w"]*255/5000),
+                    int(resp["m"]*255/5000))
             self._state = (sum(self._rgb) > 0)
 
         except:
             _LOGGER.debug("Error when reading device state: %s", resp)
 
-        return self._state
+        _LOGGER.debug("Color values: %s", str(self._rgb))
+        _LOGGER.debug("update: %s", str(self._state))
 
     @property
     def supported_features(self):
         """Return the supported features."""
-        return SUPPORT_BRIGHTNESS | SUPPORT_COLOR
+        return LightEntityFeature.EFFECT | LightEntityFeature.FLASH
+
+    @property
+    def effect_list(self):
+        """Return the supported effects."""
+        return list(EFFECTS_DICT.keys())
 
     @property
     def supported_color_modes(self):
@@ -97,23 +137,39 @@ class DoHomeLight(DoHomeDevice, LightEntity):
 
     def turn_on(self, **kwargs):
         """Turn the light on."""
+        _LOGGER.debug("Turn on params: %s", kwargs)
+
         if ATTR_RGBWW_COLOR in kwargs:
             self._rgb = kwargs[ATTR_RGBWW_COLOR]
 
         if ATTR_BRIGHTNESS in kwargs:
-            self._brightness = int(100 * kwargs[ATTR_BRIGHTNESS] / 255)
+            self._brightness = kwargs[ATTR_BRIGHTNESS]
 
         self._state = True
-        data = {
-                "cmd":6,
-                "r":int(50 * self._rgb[0] / 255)*self._brightness,
-                "g":int(50 * self._rgb[1] / 255)*self._brightness,
-                "b":int(50 * self._rgb[2] / 255)*self._brightness,
-                "w":int(50 * self._rgb[3] / 255)*self._brightness,
-                "m":int(50 * self._rgb[4] / 255)*self._brightness
-                }
-        op = json.dumps(data)
-        self._send_cmd(self._device,'cmd=ctrl&devices={[' + self._device["sid"] + ']}&op=' + op + '}', 6)
+
+        if ATTR_EFFECT in kwargs:
+            data = {
+                    "cmd":7,
+                    "index":EFFECTS_DICT[kwargs[ATTR_EFFECT]],
+                    }
+            op = json.dumps(data)
+            self._send_cmd(self._device,'cmd=ctrl&devices={[' + self._device["sid"] + ']}&op=' + op + '}', 7)
+
+        else:
+            #empty kwargs means that turn on button has pressed without any color specification
+            if len(kwargs) == 0:
+                self._rgb = (0, 0, 0, 255, 0)
+
+            data = {
+                    "cmd":6,
+                    "r":int(self._rgb[0] * self._brightness * 5000 / 65025),
+                    "g":int(self._rgb[1] * self._brightness * 5000 / 65025),
+                    "b":int(self._rgb[2] * self._brightness * 5000 / 65025),
+                    "w":int(self._rgb[3] * 5000 / 255),
+                    "m":int(self._rgb[4] * 5000 / 255)
+                    }
+            op = json.dumps(data)
+            self._send_cmd(self._device,'cmd=ctrl&devices={[' + self._device["sid"] + ']}&op=' + op + '}', 6)
 
     def turn_off(self, **kwargs):
         """Turn the light off."""
